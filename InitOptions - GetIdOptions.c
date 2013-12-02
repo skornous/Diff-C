da793dce072ff9116ed0bdc362de65f5b6729151
FlorianDeParis <florian.tournay@hotmail.fr>
2013-12-02 16:23:05 +0100

    Version finale InitOptions

--- InitOptions - GetIdOptions.c
+++ InitOptions - GetIdOptions.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "bibli.h"
 #define CODE_ERROR 0
 
-int init_options(char args**)
+int* init_options(char**);
+int getIdOptions(char*);
+
+int main(int argc, char** argv)
 {
-	int options[];
+ if (argc > 1)
+	printf("%s : %i\n",argv[1] , getIdOptions(argv[1]));
+	//printf("%i\n", getIdOptions(argv[1]));
+ else 
+	printf("ERROR\n");
+
+ return 0;
+}
+
+int* init_options(char** args)
+{
+	int* options;
 	int length;
 	int i;
 	
 	length = size(args);
 	options = malloc(sizeof(int) * length);
 	
-	for (i = 0 , i = length - 1, i++)
+	for (i = 0 ; i < length - 1; i++)
 	{
 		options[i] = getIdOptions(args[i]);
 	}
@@ -21,58 +37,60 @@ int getIdOptions(char* opt)
 {
 	char* undashedOption;
 	
-	if(opt[1] == '-')
-	{ undashedOption = substring(opt, 2, length(opt));}
-	else{
-		undashedOption = substring(opt, 1, length(opt));}
+	/*if(opt[1] == '-')
+	{undashedOption = substring(opt, 2, length(opt));}
+	else
+	{undashedOption = substring(opt, 1, length(opt));}*/
 	
-	if(strcmp(undashedOption, "normal")
+	undashedOption = opt;
+	
+	if(strcmp(undashedOption, "normal") == 0)
 	{ return 1; }
-	if(strcmp(undashedOption, "brief" || strcmp(undashedOption,"q"))
+	if(strcmp(undashedOption, "brief") == 0 || strcmp(undashedOption,"q") == 0 )
 	{ return 2; }
-	if(strcmp(undashedOption,"s") || strcmp(undashedOption, "report-identical-files"))
+	if(strcmp(undashedOption,"s") == 0 || strcmp(undashedOption, "report-identical-files") == 0 )
 	{ return 3; }
-	if(strcmp(undashedOption, "c"))
+	if(strcmp(undashedOption, "c") == 0 )
 	{ return 4; }
-	if(strcmp(undashedOption, "C") || strcmp(undashedOption, "context"))
+	if(strcmp(undashedOption, "C") == 0 || strcmp(undashedOption, "context") == 0 )
 	{ return 5; }
-	if(strcmp(undashedOption, "u"))
+	if(strcmp(undashedOption, "u") == 0 )
 	{ return 6; }
-	if(strcmp(undashedOption,"U") || strcmp(undashedOption, "unified"))
-	{ return 7; )
-	if(strcmp(undashedOption,"e")|| strcmp(undashedOption, "ed"))
+	if(strcmp(undashedOption,"U") == 0 || strcmp(undashedOption, "unified") == 0 )
+	{ return 7; }
+	if(strcmp(undashedOption,"e") == 0 || strcmp(undashedOption, "ed") == 0 )
 	{ return 8; }
-	if(strcmp(undashedOption,"n") || strcmp(undashedOption,"rcs"))
+	if(strcmp(undashedOption,"n") == 0 || strcmp(undashedOption,"rcs") == 0 )
 	{ return 9; }
-	if(strcmp(undashedOption,"y") || strcmp(undashedOption,"side-by-side"))
+	if(strcmp(undashedOption,"y") == 0 || strcmp(undashedOption,"side-by-side") == 0 )
 	{ return 10;}
-	if(strcmp(undashedOption,"w"))
+	if(strcmp(undashedOption,"w") == 0 )
 	{ return 11;}
-	if(strcmp(undashedOption,"width"))
-	{ return 12;)
-	if(strcmp(undashedOption,"left-column"))
+	if(strcmp(undashedOption,"width") == 0 )
+	{ return 12;}
+	if(strcmp(undashedOption,"left-column") == 0 )
 	{ return 13;}
-	if(strcmp(undashedOption,"suppress-common-lines"))
+	if(strcmp(undashedOption,"suppress-common-lines") == 0 )
 	{ return 14;}
-	if(strcmp(undashedOption,"p") || strcmp(undashedOption,"show-c-function"))
+	if(strcmp(undashedOption,"p") == 0 || strcmp(undashedOption,"show-c-function") == 0 )
 	{ return 15;}
-	if(strcmp(undashedOption,"F"))
+	if(strcmp(undashedOption,"F") == 0 )
 	{ return 16;}
-	if(strcmp(undashedOption,"show-function-line"))
+	if(strcmp(undashedOption,"show-function-line") == 0 )
 	{ return 17;}
-	if(strcmp(undashedOption,"t") || strcmp(undashedOption,"expand-tabs"))
+	if(strcmp(undashedOption,"t") == 0 || strcmp(undashedOption,"expand-tabs") == 0 )
 	{ return 18;}
-	if(strcmp(undashedOption,"T") || strcmp(undashedOption,"initial-tab"))
+	if(strcmp(undashedOption,"T") == 0 || strcmp(undashedOption,"initial-tab") == 0 )
 	{ return 19;}
-	if(strcmp(undashedOption,"tabsize"))
+	if(strcmp(undashedOption,"tabsize") == 0 )
 	{ return 20;}
-	if(strcmp(undashedOption,"suppress-blank-empty"))
+	if(strcmp(undashedOption,"suppress-blank-empty") == 0 )
 	{ return 21;}
-	if(strcmp(undashedOption,"l") || strcmp(undashedOption,"paginate"))
+	if(strcmp(undashedOption,"l") == 0 || strcmp(undashedOption,"paginate") == 0 )
 	{ return 22;}
-	if(strcmp(undashedOption,"ignore-file-name-case"))
+	if(strcmp(undashedOption,"ignore-file-name-case") == 0 )
 	{ return 23;}
-	if(strcmp(undashedOption,"no-ignore-file-name-case"))
+	if(strcmp(undashedOption,"no-ignore-file-name-case") == 0 )
 	{ return 24;}
 	else
 	{ return CODE_ERROR; }
