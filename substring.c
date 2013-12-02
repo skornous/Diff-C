ba9d0f8c29324cc8ad84752a4e28e398d631fc6e
Rayvexx <baptiste.flesch@gmail.com>
2013-12-02 15:23:48 +0100

    substring

---
+++ substring.c
@@ -0,0 +1,34 @@
+int main(int argc, char** argv){
+
+	substring("chaine longue", 2, 9);
+}
+
+
+char* substring(char* in, int start, int end){
+
+	char* out;
+	int j;
+	int inLength;
+	int outLength;
+
+	j = 0;
+	inLength = length(in);
+	outLength = inLength - (start+(inLength - end));
+	out = malloc(sizeof(char)*outLength);
+
+	for (int i = 0; i < (inLength-1); i++)
+	{
+		if ((i >= start) && (i <= end))
+		{
+			out[j++] = in[i];
+		}
+	}
+	return out[outLength] = "\0";
+}
+
+int length(char* str){
+	
+	int i=0;
+	while(str[i++] != '\0');
+	return i;
+}
